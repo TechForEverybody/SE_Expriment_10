5e44736257c819f0a39ece864e06274c4edb93b5
SHIVKUMAR CHAUHAN <Chauvhanshgud19ce@student.mes.ac.in>
2021-10-25 02:12:32 +0530

    function for addtion is added in the java file

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 class Main{
 	public static void main(String[] args) {
-		System.out.println("SHIVKUMAR CHAUHAN");
+		System.out.println("Author : SHIVKUMAR CHAUHAN");
+		System.out.println(add_number(5,6));
+	}
+	public static int add_number(int a,int b){
+		return a+b;
 	}
 }
